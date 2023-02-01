e822b6a72de85582c44e083a5f09ea850da0e0c9
Christian Ramirez <christian.r0602@gmail.com>
2023-01-31 21:16:01 -0600

    anagram obj algo

--- script.js
+++ script.js
@@ -95,3 +95,33 @@
 
 // compress('ccaaatsss')
 
+// Write a function, anagrams, that takes in two strings as arguments. The function should return a boolean indicating whether or not the strings are anagrams. Anagrams are strings that contain the same characters, but in any order.
+
+const anagrams = (s1, s2) => {
+  const obj = {}
+  for(let char of s1){
+    if(!(char in obj)){
+      obj[char] = 0
+    }
+    obj[char] += 1
+  }
+  
+  for(let char of s2){
+    if(char in obj){
+      obj[char] -= 1
+    }else {
+       return false
+    }
+  }
+  
+  for (let char in obj){
+    if(obj[char] !== 0){
+      return false
+    }
+  }
+  return true
+};
+
+anagrams('restful', 'fluster');
+
+
